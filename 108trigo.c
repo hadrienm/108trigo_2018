9dc1ae1ed026c20a68e8db39b1cf64d315e14902
quentin <quentin.felix@epitech.eu>
2019-03-31 06:25:01 -0400

    correction

--- 108trigo.c
+++ 108trigo.c
@@ -299,12 +299,14 @@ int verify_h(char *str)
 
 int main(int ac, char **av)
 {
+    double pass = sqrt(ac  - 2);
+    int pass2 = (int)(sqrt(ac - 2));
     /*double **res = menos((double *[2]){(double[2]){1, 2}, (double[2]){3, 4}}, (double *[2]){(double[2]){1, 2}, (double[2]){3, 4}}, ac);*/
     char tab[5][5] = {"COS", "EXP", "SIN", "COSH", "SINH"};
     int res = 0;
     if (ac == 2 && verify_h(av[1]) == 0)return 0;
     if (ac < 3)return 84;
-    if (sqrt(ac - 2) % 2 != 0)return 84;
+    if (pass != (float)pass2)return 84;
     for (int i = 0; i != 5; ++i)if (comp(tab[i], av[1]) == 0)res++;
     if (res == 1);
     else return 84;
